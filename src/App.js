79e949ec342b15dac0500f8565db453d155e0f70
Adrien Imberdis <AdrienImberdis@gmail.com>
2019-10-08 11:27:01 +0200

    Add components folder w/ chat-app.css & Contact.js + App.js modified

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Contact
+      image = "https://randomuser.me/api/portraits/women/20.jpg"
+      name = "Felecia Hunter"
+      status = "true"
+      />  
+
+      <Contact
+      image = "https://randomuser.me/api/portraits/women/54.jpg"
+      name = "Lucille Henderson"
+      status = "true"
+      />
+
+      <Contact
+      image = "https://randomuser.me/api/portraits/men/89.jpg"
+      name = "Nelson Holland"
+      status = ""
+      />
+
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Chat-app.css';
+
+
+function Contact (props) {
+    return(
+
+        <div class="Contact">
+            <img
+            src= {props.image} 
+            
+            alt={props.name} class="avatar" />
+
+            <div>
+
+                <h4 class="name">{props.name}</h4>
+
+                <div class="status">
+
+                <div class={props.status ? 'status-online' : 'status-offline'}></div>
+                <div class="status-text">{props.status ? "Online" : "Offline"} </div>
+                </div>
+
+            </div>
+        </div>
+
+    );
+};
+
+export default Contact; 
